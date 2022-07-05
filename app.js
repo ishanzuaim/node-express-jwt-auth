7eeabf9b3387c279d6cfd48d064baf113260ab83
Ishan Zuaim <ishanzuaim@gmail.com>
2022-07-05 22:36:41 +1000

    Test cookies.

--- app.js
+++ app.js
@@ -2,12 +2,15 @@ const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const auth = require("./routes/auth");
+const cookieParser = require("cookie-parser");
+
 dotenv.config();
 const app = express();
 
 // middleware
 app.use(express.static("public"));
 app.use(express.json());
+app.use(cookieParser());
 
 // view engine
 app.set("view engine", "ejs");
@@ -27,3 +30,18 @@ mongoose
 app.get("/", (req, res) => res.render("home"));
 app.get("/smoothies", (req, res) => res.render("smoothies"));
 app.use(auth);
+
+app.get("/set-cookies", (req, res) => {
+  // res.setHeader("Set-Cookie", "newUser=true");
+
+  res.cookie("newUser", false);
+  res.cookie("isEmployee", true, { httpOnly: true });
+
+  res.send("cookie delivered");
+});
+
+app.get("/read-cookies", (req, res) => {
+  console.log(cookies);
+
+  res.json(cookies);
+});
