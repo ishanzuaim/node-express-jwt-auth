1cec1ff9db668736838667fe16e06579e7909476
Ishan Zuaim <ishanzuaim@gmail.com>
2022-07-10 19:34:53 +1000

    added conditional rendering and logout.

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const auth = require("./routes/auth");
 const cookieParser = require("cookie-parser");
-const { requireAuth } = require("./middleware/authMiddleware");
+const { requireAuth, checkUser } = require("./middleware/authMiddleware");
 dotenv.config();
 const app = express();
 
@@ -27,6 +27,7 @@ mongoose
   .catch((err) => console.log(err));
 
 // routes
+app.get("*", checkUser);
 app.get("/", (req, res) => res.render("home"));
 app.get("/smoothies", requireAuth, (req, res) => res.render("smoothies"));
 app.use(auth);
--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,4 +1,5 @@
 const jwt = require("jsonwebtoken");
+const User = require("../models/User");
 
 const requireAuth = (req, res, next) => {
   const token = req.cookies.jwt;
@@ -16,6 +17,25 @@ const requireAuth = (req, res, next) => {
   }
 };
 
+const checkUser = (req, res, next) => {
+  const token = req.cookies.jwt;
+  if (!token) {
+    res.locals.user = null;
+    next();
+  }
+  jwt.verify(token, "secret value", async (err, decodedToken) => {
+    if (err) {
+      res.locals.user = null;
+      next();
+    } else {
+      const user = await User.findById(decodedToken.id);
+      res.locals.user = user;
+      next();
+    }
+  });
+};
+
 module.exports = {
   requireAuth,
+  checkUser,
 };
